38beb0118640a5f2fb5ed8611e3b84f7c693b848
Anton Schegg <anton@schegg.de>
2024-01-19 16:51:25 +0100

    Erzeuge DB-Schema nur, wenn noch nicht in aktueller Version.

--- tests/database.test.js
+++ tests/database.test.js
@@ -1,61 +1,65 @@
+import { DateTime } from 'luxon';
+import _ from 'lodash';
+import { expect } from 'chai';
 import DB from '../lib/database.js';
 import AsExpress from '../lib/as-express.js';
 import config from '../lib/config.js';
-import {DateTime} from "luxon";
-import _ from "lodash";
-import {expect} from 'chai';
 
-const dbSchema = [
-  {
-    "tableName": "Test1",
-    "columns": [
-      {
-        "name": "id",
-        "type": "autoincrement",
-        "primary_key": true,
-      },
-      {
-        "name": "TestUniqueString",
-        "type": "string",
-        "unique": true,
-        "nullable": false,
-      },
-    ],
-  },
-];
+const dbSchema = {
+  version: 1,
+  name: 'test',
+  tables: [
+    {
+      tableName: 'Test1',
+      columns: [
+        {
+          name: 'id',
+          type: 'autoincrement',
+          primary_key: true,
+        },
+        {
+          name: 'TestUniqueString',
+          type: 'string',
+          unique: true,
+          nullable: false,
+        },
+      ],
+    },
+  ],
+};
 
 const permissions = {
   test1_list_read: {
-    description: "TestUniqueString anzeigen",
-    resources: ["/api/v1/gattung"],
-    permissions: ["get"],
-    menus: ["admin.gattungen"]
+    description: 'TestUniqueString anzeigen',
+    resources: ['/api/v1/gattung'],
+    permissions: ['get'],
+    menus: ['admin.gattungen'],
   },
   test1_create: {
-    description: "Neuen TestUniqueString anlegen",
-    resources: ["/api/v1/gattung"],
-    permissions: ["put"],
-    menus: ["admin.gattung"]
+    description: 'Neuen TestUniqueString anlegen',
+    resources: ['/api/v1/gattung'],
+    permissions: ['put'],
+    menus: ['admin.gattung'],
   },
   test1_read: {
-    description: "TestUniqueString anzeigen",
-    resources: ["/api/v1/gattung/:id"],
-    permissions: ["get"],
-    menus: ["admin.gattung"]
+    description: 'TestUniqueString anzeigen',
+    resources: ['/api/v1/gattung/:id'],
+    permissions: ['get'],
+    menus: ['admin.gattung'],
   },
   test1_update: {
-    description: "TestUniqueString ändern",
-    resources: ["/api/v1/gattung/:id"],
-    permissions: ["get", "post"],
-    menus: ["admin.gattung"]
+    description: 'TestUniqueString ändern',
+    resources: ['/api/v1/gattung/:id'],
+    permissions: ['get', 'post'],
+    menus: ['admin.gattung'],
   },
   test1_delete: {
-    description: "TestUniqueString löschen",
-    resources: ["/api/v1/gattung/:id"],
-    permissions: ["delete"],
-    menus: ["admin.gattung"]
+    description: 'TestUniqueString löschen',
+    resources: ['/api/v1/gattung/:id'],
+    permissions: ['delete'],
+    menus: ['admin.gattung'],
   },
-}
+};
 
 class App {
   #kv = {};
@@ -74,70 +78,67 @@ const app = new App();
 
 const asExpress = new AsExpress('as-express-test', app);
 
-after(function(){
+after(() => {
   console.log('Testing finished. Terminating http server');
   asExpress.terminateHttpServer();
 });
 
-describe('Testing the Database Functions', function() {
-
-
+describe('Testing the Database Functions', () => {
   let db;
   let existingUserIds = [];
   const usersToDelete = [];
-  let oldPw, newPw, userId;
+  let oldPw; let newPw; let
+    userId;
 
-  it('Initializing AsExpress', async function () {
+  it('asExpress.init', async () => {
     await asExpress.init({
-      dbSchema: dbSchema,
-      permissions: permissions,
+      dbSchemas: [dbSchema],
+      permissions,
     });
   });
 
-  it('creating db instance', function (done) {
-    db = new DB({appName: asExpress.appName});
+  it('creating db instance', (done) => {
+    db = new DB({ appName: asExpress.appName });
     expect(db).to.not.be.undefined;
     done();
   });
 
-  it('reading already existing users', async function () {
-    existingUserIds = _.map(await db.getUser(), (user)=> {
-      return user.id;
-    });
+  it('reading already existing users', async () => {
+    existingUserIds = _.map(await db.getUser(), (user) => user.id);
   });
 
-  it('creating users', async function () {
+  it('creating users', async () => {
     userId = await db.createUser('joe@example.com', 'honk123;Z');
     expect(userId).to.not.be.undefined;
-    expect(userId).to.be.a('number')
+    expect(userId).to.be.a('number');
     oldPw = '93h8tgqi';
     userId = await db.createUser('joe@example.com', oldPw);
     usersToDelete.push(userId);
     expect(userId).to.not.be.undefined;
-    expect(userId).to.be.a('number')
+    expect(userId).to.be.a('number');
   });
 
-  it('set EmailConfirmed to true and try creating user with same email again, which must fail', async function () {
-    await db.updateUserById(userId, {EmailConfirmed: true})
+  it('set EmailConfirmed to true and try creating user with same email again, which must fail', async () => {
+    await db.updateUserById(userId, { EmailConfirmed: true });
 
     let ok;
     try {
       const userId = await db.createUser('joe@example.com', '98igoauhg9');
       ok = false; // must not come here, because the createUser call must throw an exception
-    } catch(ex) {
+    } catch (ex) {
       ok = ex.cause === 'exists';
     }
     expect(ok).to.be.true;
   });
 
-  it('update password and try login', async function () {
+  it('update password and try login', async () => {
     let ok;
     newPw = '&lg652!2dg';
-    await db.updateUserById(userId, {Password: newPw});
+    await db.updateUserById(userId, { Password: newPw });
     try {
       await db.validateUser('joe@example.com', oldPw);
       ok = false; // must not come here, because it is expected that validateUser fails due to wrong pw
-    } catch(ex) {
+    } catch (ex) {
       ok = ex.cause === 'invalid';
     }
     expect(ok).to.be.true;
@@ -145,26 +146,26 @@ describe('Testing the Database Functions', function() {
     try {
       await db.validateUser('joe@example.com', newPw);
       ok = true; // must come here, because it is expected that validateUser succeeds with correct
-    } catch(ex) {
-        ok = false; // must not fail with wrong password or any other error
+    } catch (ex) {
+      ok = false; // must not fail with wrong password or any other error
     }
     expect(ok).to.be.true;
   });
 
-  it('update expiration date and try login', async function () {
+  it('update expiration date and try login', async () => {
     let ok;
     // check expired user fails validation
-    await db.updateUserByEmail('joe@example.com', {ExpiredAfter: DateTime.now().minus({'minutes': 1})});
+    await db.updateUserByEmail('joe@example.com', { ExpiredAfter: DateTime.now().minus({ minutes: 1 }) });
     try {
       await db.validateUser('joe@example.com', newPw);
       ok = false; // Validation did not fail for expired user
-    } catch(reason) {
+    } catch (reason) {
       ok = reason.cause === 'expired';
     }
     expect(ok).to.be.true;
   });
 
-  it('Check getUserByEmail', async function () {
+  it('Check getUserByEmail', async () => {
     userId = await db.createUser('nick@example.com', 'lagrjpoa@gagj');
     usersToDelete.push(userId);
     const u2 = await db.getUserByEmail('joe@example.com');
@@ -186,7 +187,7 @@ describe('Testing the Database Functions', function() {
     expect(u3b.Email).to.equal('nick@example.com');
   });
 
-  it('Check deleteUsers', async function () {
+  it('Check deleteUsers', async () => {
     userId = await db.createUser('Karin@example.com', 'Karin Nubrowski');
     usersToDelete.push(userId);
     userId = await db.createUser('jeff@example.com', 'Jeff Amazini');
@@ -196,13 +197,9 @@ describe('Testing the Database Functions', function() {
     const u3dd = await db.deleteUsers(usersToDelete);
     expect(u3dd).to.equal(3);
     const leftUsers = await db.getUser();
-    const leftUsersIds = _.map(leftUsers, (user)=> {
-      return user.id;
-    });
+    const leftUsersIds = _.map(leftUsers, (user) => user.id);
 
-    let difference = existingUserIds.filter(x => !leftUsersIds.includes(x));
+    const difference = existingUserIds.filter((x) => !leftUsersIds.includes(x));
     expect(difference.length).to.equal(0);
   });
-
 });
-
